aee06cf7f2626434877656a1ef5b2c902613ba6b
Viswa <viswa.bharathi@bofin.com>
2018-11-21 19:24:39 +0530

    intial commit

---
+++ app/src/main/java/com/phonecontact_demo/Contact_Adapter.java
@@ -0,0 +1,172 @@
+package com.phonecontact_demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class Contact_Adapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Contact_Model> arrayList;
+    private boolean isProcessCompleted = false;
+
+    public Contact_Adapter(Context context) {
+        this.context = context;
+        arrayList = new ArrayList<>();
+    }
+
+    public void setCommonlist(ArrayList<Contact_Model> commonlist, boolean isProcessCompleted) {
+        if (commonlist == null)
+            return;
+        arrayList.clear();
+        arrayList.addAll(commonlist);
+        this.isProcessCompleted = isProcessCompleted;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+
+        return arrayList.size();
+    }
+
+    @Override
+    public Contact_Model getItem(int position) {
+
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Contact_Model model = arrayList.get(position);
+        ViewHodler holder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.custom_view, parent, false);
+            holder = new ViewHodler();
+            holder.contactImage = (ImageView) convertView
+                    .findViewById(R.id.contactImage);
+            holder.contactName = (TextView) convertView
+                    .findViewById(R.id.contactName);
+            holder.contactEmail = (TextView) convertView
+                    .findViewById(R.id.contactEmail);
+            holder.contactNumber = (TextView) convertView
+                    .findViewById(R.id.contactNumber);
+            holder.contactOtherDetails = (TextView) convertView
+                    .findViewById(R.id.contactOtherDetails);
+            holder.progressBar = (ProgressBar) convertView
+                    .findViewById(R.id.pr_loader);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHodler) convertView.getTag();
+        }
+
+        if (model != null) {
+
+            // Set items to all view
+            if (model.getContactName() != null && !model.getContactName().equals("")
+                    && model.getContactName() != null) {
+                holder.contactName.setText(model.getContactName());
+            } else {
+                holder.contactName.setText("No Name");
+            }
+            if (model.getContactEmail() != null && !model.getContactEmail().equals("")
+                    && model.getContactEmail() != null) {
+                holder.contactEmail.setText("EMAIL ID - \n"
+                        + model.getContactEmail());
+            } else {
+                holder.contactEmail.setText("EMAIL ID - \n" + "No EmailId");
+            }
+
+            if (model.getContactNumber() != null && !model.getContactNumber().equals("")
+                    && model.getContactNumber() != null) {
+                holder.contactNumber.setText("CONTACT NUMBER - \n"
+                        + model.getContactNumber());
+            } else {
+                holder.contactNumber.setText("CONTACT NUMBER - \n"
+                        + "No Contact Number");
+            }
+
+            if (model.getContactOtherDetails() != null && !model.getContactOtherDetails().equals("")
+                    && model.getContactOtherDetails() != null) {
+                holder.contactOtherDetails.setText("OTHER DETAILS - \n"
+                        + model.getContactOtherDetails());
+            } else {
+                holder.contactOtherDetails.setText("OTHER DETAILS - \n"
+                        + "Other details are empty");
+            }
+
+            // Bitmap for imageview
+            Bitmap image = null;
+            if (model.getContactPhoto() != null && !model.getContactPhoto().equals("")
+                    && model.getContactPhoto() != null) {
+                image = BitmapFactory.decodeFile(model.getContactPhoto());// decode
+                // the
+                // image
+                // into
+                // bitmap
+                if (image != null)
+                    if (isProcessCompleted) {
+                        holder.contactImage.setImageBitmap(image);// Set image if bitmap
+                        holder.contactImage.setVisibility(View.VISIBLE);
+                        holder.progressBar.setVisibility(View.GONE);
+                    } else {
+                        holder.contactImage.setVisibility(View.GONE);
+                        holder.progressBar.setVisibility(View.VISIBLE);
+                    }
+                    // is not null
+                else {
+                    image = BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.androhub_logo);// if bitmap is null then set
+                    // default bitmap image to
+                    // contact image
+                    if (isProcessCompleted) {
+                        holder.contactImage.setImageBitmap(image);
+                        holder.contactImage.setVisibility(View.VISIBLE);
+                        holder.progressBar.setVisibility(View.GONE);
+                    } else {
+                        holder.contactImage.setVisibility(View.GONE);
+                        holder.progressBar.setVisibility(View.VISIBLE);
+                    }
+                }
+            } else {
+                image = BitmapFactory.decodeResource(context.getResources(),
+                        R.drawable.androhub_logo);
+                if (isProcessCompleted) {
+                    holder.contactImage.setVisibility(View.VISIBLE);
+                    holder.progressBar.setVisibility(View.GONE);
+                    holder.contactImage.setImageBitmap(image);
+                } else {
+                    holder.contactImage.setVisibility(View.GONE);
+                    holder.progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+        return convertView;
+    }
+
+    // View holder to hold views
+    private class ViewHodler {
+        ImageView contactImage;
+        TextView contactName, contactNumber, contactEmail, contactOtherDetails;
+        ProgressBar progressBar;
+    }
+}
